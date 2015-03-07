b3d46e7a6295cb61c0b4070e5577baf4b5b9a82c
Stephen Tate <accounts@sjtate.co.uk>
2015-03-07 16:35:12 +0000

    Fixing semi-colon errors, php it has been a while...

--- databaseConnection.php
+++ databaseConnection.php
@@ -1,21 +1,21 @@
 <?php
 	
 	class databaseConnection extends PDO {
-		function connectToDatabase() 
-			$host = 'dragon.kent.ac.uk'
-			$dbname = 'sjt43'
-			$user = 'sjt43'
-			$password = 'octium!'
+		function connectToDatabase() {
+			$host = 'dragon.kent.ac.uk';
+			$dbname = 'sjt43';
+			$user = 'sjt43';
+			$password = 'octium!';
 
 			try {
 				$dbConnection = new PDO ('mysql:host=$host;dbname=$dbname', $user, $password);
-				return $dbConnection
+				return $dbConnection;
 			}
-			catch (PDOException $exception){
-				return "Unable to connect to the database"
+			catch (PDOException $exception) {
+				return "Unable to connect to the database";
 			}
 		}
 	}
 
-	echo connectToDatabase()
+	echo connectToDatabase();
 ?>
