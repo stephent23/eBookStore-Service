61e07c42bba0c58a62c94c36e970587ead0a7b0c
Stephen Tate <accounts@sjtate.co.uk>
2015-03-07 16:29:49 +0000

    adding the database connection

---
+++ databaseConnection.php
@@ -0,0 +1,21 @@
+<?php
+	
+	class databaseConnection extends PDO {
+		function connectToDatabase() 
+			$host = 'dragon.kent.ac.uk'
+			$dbname = 'sjt43'
+			$user = 'sjt43'
+			$password = 'octium!'
+
+			try {
+				$dbConnection = new PDO ('mysql:host=$host;dbname=$dbname', $user, $password);
+				return $dbConnection
+			}
+			catch (PDOException $exception){
+				return "Unable to connect to the database"
+			}
+		}
+	}
+
+	echo connectToDatabase()
+?>
